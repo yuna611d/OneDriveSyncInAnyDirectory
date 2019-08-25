17a8ffd81b2440f1df34c953894e08ea3f1d6fd4
yuna611d <yuna611d@gmail.com>
2019-08-25 21:58:48 +0900

    [WIP] current code is not working. error: Tenant does not have a SPO license."

--- cmd/api/onedriveapi.go
+++ cmd/api/onedriveapi.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"io/ioutil"
 
 	// TODO tentative
 	"../models"
@@ -29,16 +30,46 @@ func callOneDriveAPI(apiURI string) io.ReadCloser {
 		os.Exit(1)
 	}
 	defer res.Body.Close()
+
+	body, error := ioutil.ReadAll(res.Body)
+	if error != nil {
+		fmt.Println("err " )		
+	}
+	fmt.Println("[body] " + string(body))
+
+
 	return res.Body
 }
 
 func GetOneDriveRootDir() models.OneDriveDriveItems {
-	apiURI := "https://graph.microsoft.com/v1.0/users/me/drive/root/childrens"
+	// apiURI := "https://graph.microsoft.com/v1.0/users/me/drive/root/childrens"
+	apiURI := "https://graph.microsoft.com/v1.0/me/drive/root/children"
 
-	responseBody := callOneDriveAPI(apiURI)
-	// responseBody.Close()
+	client := http.Client{}
+	req, err := http.NewRequest("GET", apiURI, nil)
+	if err != nil {
+		// TODO error handle
+		fmt.Printf("request for api is failed \n")
+		os.Exit(1)
+	}
+	req.Header.Set("Authorization", "bearer "+ auth.GetInstance().AccessToken)
+	res, err := client.Do(req)
+	if err != nil {
+		// TODO error handle
+		fmt.Printf("response from api is failed \n")
+		os.Exit(1)
+	}
+	defer res.Body.Close()
+
+	body, error := ioutil.ReadAll(res.Body)
+	if error != nil {
+		fmt.Println("err " )		
+	}
+	fmt.Println("[body] " + string(body))
+
+
 	target := models.OneDriveDriveItems{}
-	getJSON(responseBody, &target)
+	getJSON(res.Body, &target)
 	return target
 }
 
